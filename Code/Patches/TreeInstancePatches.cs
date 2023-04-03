0b58e1c31581340536c0e34aaf435b32b40626d2
algernon-A <aeneas@aloku.net>
2023-04-03 22:47:08 +1000

    Implement random tree rotation.

---
+++ Code/Patches/TreeInstancePatches.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="TreeInstancePatches.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace TreeControl.Patches
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using AlgernonCommons;
+    using HarmonyLib;
+    using UnityEngine;
+
+    /// <summary>
+    /// Harmony patches to implement random tree rotation.
+    /// </summary>
+    [HarmonyPatch(typeof(global::TreeInstance))]
+    public static class TreeInstancePatches
+    {
+        /// <summary>
+        /// Harmony transpiler for TreeInstance.RenderInstance to implement random tree rotation.
+        /// </summary>
+        /// <param name="instructions">Original ILCode.</param>
+        /// <returns>Modified ILCode.</returns>
+        [HarmonyPatch(nameof(global::TreeInstance.RenderInstance), new Type[] { typeof(RenderManager.CameraInfo), typeof(TreeInfo), typeof(Vector3), typeof(float), typeof(float), typeof(Vector4), typeof(bool) })]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> RenderInstanceTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            // Quaternion.identity getter.
+            MethodInfo qIdentity = AccessTools.PropertyGetter(typeof(Quaternion), nameof(Quaternion.identity));
+
+            // Looking for new Quaternion.identity getter call.
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.Calls(qIdentity))
+                {
+                    // Found it - drop original call and reokace with our custom method.
+                    Logging.Message("found Quaternion.identity");
+                    yield return new CodeInstruction(OpCodes.Ldarg_2);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(TreeInstancePatches), nameof(TreeRotation)));
+                    continue;
+                }
+
+                yield return instruction;
+            }
+        }
+
+        /// <summary>
+        /// Random tree rotation calculator.
+        /// </summary>
+        /// <param name="location">Tree location.</param>
+        /// <returns>Calculated rotation quaternion.</returns>
+        private static Quaternion TreeRotation(Vector3 location) => Quaternion.Euler(0, ((location.x * location.x) + (location.z * location.z)) % 359, 0);
+    }
+}
